b5b80b03b0e9dd71d7221c55d23d7c7abda4288d
Fleetwood <john.fleetwood@onepeloton.com>
2019-09-10 11:56:12 -0400

    Added login.html and Bootstrap + Nav. WIP: Form submit.

--- blog_api/manage.py
+++ blog_api/manage.py
@@ -1,7 +1,6 @@
 import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
 from src.app import create_app, db
 
 env_name = os.getenv('FLASK_ENV')
@@ -15,5 +14,3 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
-
-    
